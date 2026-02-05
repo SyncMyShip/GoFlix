a2ca430d57ea862298ff65520d6df88bc27016ec
JD <francis.jadec@gmail.com>
2026-02-04 20:36:27 -0500

    delete movie

--- main.go
+++ main.go
@@ -41,6 +41,21 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movie)
 }
 
+// DELETE: delete a single movie
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+	
+}
+
 
 func main() {
 	r := mux.NewRouter()
@@ -49,7 +64,7 @@ func main() {
 	// r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
-	// r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("starting server on port 8000...")
 	log.Fatal(http.ListenAndServe(":8000", r))
